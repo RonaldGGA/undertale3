76d164f909c7bb3f60d3385e6029c4713b4f61c5
RonaldGGA <ronald.dearmass@gmail.com>
2024-03-07 21:15:54 -0500

    xd

--- src/components/NewFeaturesBar/Feature.tsx
+++ src/components/NewFeaturesBar/Feature.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import feature1 from "../../assets/feature/feature1.png";
+import feature2 from "../../assets/feature/feature2.png";
+import feature3 from "../../assets/feature/feature3.png";
 
 interface featureProps {
   title: string;
@@ -9,7 +12,21 @@ const Feature: React.FC<featureProps> = ({ title, currentIndex }) => {
   return (
     <div
       className="app__landing__newFeatures-feature"
-      style={{ transform: `translateX(${currentIndex * -100}%)` }}>
+      style={{
+        transform: `translateX(${currentIndex * -100}%)`,
+        background: `linear-gradient(
+          to bottom,
+          rgba(0 0 0 /0.5),
+          rgba(0 0 0 /0.6)
+        ),
+        url(${
+          currentIndex % 3 === 0
+            ? feature1
+            : currentIndex % 3 === 1
+            ? feature2
+            : feature3
+        })`,
+      }}>
       <div className="app__landing__newFeatures-feature-text">
         <p>{title}</p>
         <a href="#">Learn More</a>
